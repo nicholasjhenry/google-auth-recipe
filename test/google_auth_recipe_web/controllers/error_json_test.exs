8a972a4b3ffb7e1549a9f8ce371edef32b1a8e50
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2026-03-07 17:10:51 -0500

    style: Format

--- test/google_auth_recipe_web/controllers/error_json_test.exs
+++ test/google_auth_recipe_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule GoogleAuthRecipeWeb.ErrorJSONTest do
   use GoogleAuthRecipeWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert GoogleAuthRecipeWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert GoogleAuthRecipeWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
